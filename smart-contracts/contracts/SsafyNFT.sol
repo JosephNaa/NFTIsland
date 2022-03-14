7a374e3c3e3d44ef49f1d6224797da5e9b8671fe
kgw012 <kgw012@gmail.com>
2022-03-14 15:29:51 +0900

    [Add] ERC721, SssafyNFT

--- smart-contracts/contracts/SsafyNFT.sol
+++ smart-contracts/contracts/SsafyNFT.sol
@@ -7,24 +7,30 @@ import "./token/ERC721/ERC721.sol";
  * PJT Ⅰ - 과제 2) NFT Creator 구현
  * 상태 변수나 함수의 시그니처는 구현에 따라 변경할 수 있습니다.
  */
-contract SsafyNFT {
+contract SsafyNFT is ERC721 {
 
     uint256 private _tokenIds;
     mapping(uint256 => string) tokenURIs;
 
-    constructor() {
-        // TODO
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+        _tokenIds = 0;
     }
 
     function current() public view returns (uint256) {
         return _tokenIds;
     }
 
-    function tokenURI(uint256 tokenId) public view returns (string memory) {
-        // TODO
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        return tokenURIs[tokenId];
     }
 
-    function create(address to, string memory _tokenURI) public returns (uint256) {
-        // TODO
+    function create(address to, string memory _tokenURI) public virtual returns (uint256) {
+        uint256 tokenId = _tokenIds;
+        _mint(to, tokenId);
+        tokenURIs[tokenId] = _tokenURI;
+        
+        _tokenIds += 1;
+
+        return tokenId;
     }
 }
