6732221cd03a32b7f3db26a966dc37b6a1f1320c
hhhhhhdong <gusehd520@gmail.com>
2022-03-17 16:16:29 +0900

    Add 메타마스크 로그인 연동

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,34 @@
+import { useEffect, useMemo, useState } from 'react';
 import Router from './routes';
 import ThemeConfig from './theme';
 import GlobalStyles from './theme/globalStyles';
+import UserContext from './context/UserContext';
 
 export default function App() {
+	const [accounts, setAccounts] = useState('');
+	const getAccounts = async () => {
+		const userAccounts = await window.ethereum.request({
+			method: 'eth_accounts',
+		});
+		if (userAccounts[0]) {
+			setAccounts(userAccounts[0]);
+		} else {
+			setAccounts('');
+		}
+	};
+	useEffect(async () => {
+		window.ethereum.on('accountsChanged', async () => {
+			getAccounts();
+		});
+		getAccounts();
+	}, []);
+
 	return (
 		<ThemeConfig>
 			<GlobalStyles />
-			<Router />
+			<UserContext.Provider value={accounts}>
+				<Router />
+			</UserContext.Provider>
 		</ThemeConfig>
 	);
 }
--- frontend/src/layouts/main/DashboardNavbar.jsx
+++ frontend/src/layouts/main/DashboardNavbar.jsx
@@ -1,10 +1,13 @@
+import { useContext } from 'react';
 import { alpha, styled } from '@mui/material/styles';
 import { Box, Stack, Button, AppBar, Toolbar } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
 import Logo from '../../components/Logo';
+import UserContext from '../../context/UserContext';
 
 // 헤더 화면 (상단 메뉴바)
 function DashboardNavbar() {
+	const user = useContext(UserContext);
 	const APPBAR_MOBILE = 64;
 	const APPBAR_DESKTOP = 92;
 
@@ -22,6 +25,28 @@ function DashboardNavbar() {
 			padding: theme.spacing(0, 5),
 		},
 	}));
+
+	const getAccount = async () => {
+		try {
+			if (window.ethereum) {
+				await window.ethereum.request({
+					method: 'eth_requestAccounts',
+				});
+			} else {
+				alert('Install Metamask!');
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	const onClickLogin = () => {
+		getAccount();
+	};
+	const onClickLogout = () => {
+		// userContext의 값 비우기
+		console.log('logout');
+	};
 
 	return (
 		<RootStyle>
@@ -64,6 +89,15 @@ function DashboardNavbar() {
 					>
 						후즈컬렉션
 					</Button>
+					{user ? (
+						<Button size='large' sx={{ fontSize: 17 }} onClick={onClickLogout}>
+							로그아웃
+						</Button>
+					) : (
+						<Button size='large' sx={{ fontSize: 17 }} onClick={onClickLogin}>
+							로그인
+						</Button>
+					)}
 				</Stack>
 			</ToolbarStyle>
 		</RootStyle>
