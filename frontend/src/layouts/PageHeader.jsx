9739355f8b5c0fcd909398a6296244883e320eb2
hhhhhhdong <gusehd520@gmail.com>
2022-03-25 14:57:42 +0900

    Add CreateCommunity Form

--- frontend/src/layouts/PageHeader.jsx
+++ frontend/src/layouts/PageHeader.jsx
@@ -2,13 +2,13 @@ import { Stack, Box, Typography } from '@mui/material';
 import { ArrowBack as BackIcon } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
 
-function PageHeader({ title }) {
+function PageHeader({ title, ...other }) {
 	const navigate = useNavigate();
 	const onClickBackIcon = () => {
 		navigate(-1);
 	};
 	return (
-		<Stack justifyContent='center' direction='row' position='relative'>
+		<Stack justifyContent='center' direction='row' position='relative' {...other}>
 			<Box position='absolute' left='0' top='12px' sx={{ cursor: 'pointer' }}>
 				<BackIcon onClick={onClickBackIcon} />
 			</Box>
--- frontend/src/pages/CreateCommunity.jsx
+++ frontend/src/pages/CreateCommunity.jsx
@@ -1,34 +1,146 @@
-import { Container, Stack, Box, Typography } from '@mui/material';
+import { useState, useRef } from 'react';
+import {
+	Container,
+	Stack,
+	Box,
+	Typography,
+	TextField,
+	Divider,
+	Button,
+} from '@mui/material';
 import { Form, FormikProvider, useFormik } from 'formik';
 import * as Yup from 'yup';
 import Header from '../layouts/PageHeader';
 import Page from '../components/Page';
 
 function CreateCommunity() {
-	// 타이핑 헬퍼
-	// const typeSchema = Yup.object().shape({
-	// 	author: Yup.string().required('커뮤니티이름 입력해주세요.'),
-	// 	title: Yup.string().required('커뮤니티 설명을 입력해주세요.'),
-	// });
+	const imageSelect = useRef();
+	const [imgae, setImage] = useState('');
+	const [imageName, setImageName] = useState('');
+	const [communityName, setCommunityName] = useState('');
+	const [description, setDescription] = useState('');
 
-	// const formik = useFormik({
-	// 	initialValues: {
-	// 		author: '',
-	// 		title: '',
-	// 		description: '',
-	// 	},
-	// 	validationSchema: typeSchema,
-	// 	onSubmit: value => {
-	// 		setAuthor(value.author);
-	// 		setTitle(value.title);
-	// 		setDescription(value.description);
-	// 	},
-	// });
+	// 타이핑 헬퍼
+	const typeSchema = Yup.object().shape({
+		communityName: Yup.string().required('커뮤니티이름 입력해주세요.'),
+		description: Yup.string().required('커뮤니티 설명을 입력해주세요.'),
+	});
+
+	const formik = useFormik({
+		initialValues: {
+			communityName: '',
+			description: '',
+		},
+		validationSchema: typeSchema,
+		onSubmit: value => {
+			setCommunityName(value.communityName);
+			setDescription(value.description);
+			// 여기서 백엔드호출해서 커뮤니티 만들기
+
+			// onClick={
+			// 	Object.keys(touched).length &&
+			// 	!Object.keys(errors).length &&
+			// 	item.length !== 0
+			// 		? toggleApprove
+			// 		: null
+			// }
+		},
+	});
+
+	const { errors, touched, handleSubmit, handleReset, getFieldProps } = formik;
+
+	// 찾기 버튼 클릭 핸들링
+	const handleImageClick = () => {
+		imageSelect.current.click();
+	};
+
+	// 이미지 업로드 핸들링
+	const handleImage = value => {
+		console.log(value);
+		// S3에 이미지 보내고 URL받아오기
+		setImage(value);
+
+		if (value !== '') setImageName(value.name);
+		else setImageName('');
+	};
 
 	return (
 		<Page>
 			<Container maxWidth='md'>
-				<Header title='커뮤니티 생성' />
+				<Header title='커뮤니티 생성' sx={{ marginBottom: '30px' }} />
+				<FormikProvider value={formik}>
+					<Form
+						autoComplete='off'
+						noValidate
+						onSubmit={handleSubmit}
+						onReset={handleReset}
+					>
+						<Stack spacing={3}>
+							<Stack direction='row' alignItems='center'>
+								<input
+									type='file'
+									accept='image/png, image/jpeg, image/jpg, image/gif'
+									ref={imageSelect}
+									onChange={e =>
+										e.target.files.length !== 0
+											? handleImage(e.target.files[0])
+											: handleImage('')
+									}
+									style={{ display: 'none' }}
+								/>
+								<TextField
+									sx={{ width: '85%' }}
+									type='text'
+									label='아이템 (업로드 확장자 형식: png, jpeg, jpg, gif)'
+									value={imageName}
+									disabled
+								/>
+								<Button
+									sx={{
+										ml: 3,
+										fontSize: 16,
+										height: '56px',
+										width: '15%',
+										padding: '8px 0',
+									}}
+									variant='contained'
+									size='large'
+									onClick={handleImageClick}
+								>
+									Upload
+								</Button>
+							</Stack>
+							<TextField
+								inputProps={{ maxLength: 20 }}
+								type='text'
+								label='이름'
+								{...getFieldProps('communityName')}
+								error={Boolean(touched.communityName && errors.communityName)}
+								helperText={touched.communityName && errors.communityName}
+							/>
+							<TextField
+								inputProps={{ maxLength: 20 }}
+								type='text'
+								label='설명'
+								{...getFieldProps('description')}
+								error={Boolean(touched.description && errors.description)}
+								helperText={touched.description && errors.description}
+							/>
+
+							<Divider sx={{ mt: 5 }} />
+							<Stack alignItems='center'>
+								<Button
+									sx={{ width: '40%', fontSize: 18 }}
+									size='large'
+									type='submit'
+									variant='contained'
+								>
+									Create
+								</Button>
+							</Stack>
+						</Stack>
+					</Form>
+				</FormikProvider>
 			</Container>
 		</Page>
 	);
