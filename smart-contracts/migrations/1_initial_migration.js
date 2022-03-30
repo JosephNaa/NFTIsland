2b6e77030f520164a9c5bfaff8570b36b3b1c211
kgw012 <kgw012@gmail.com>
2022-03-30 13:33:39 +0900

    Add ropsten network

--- smart-contracts/migrations/1_initial_migration.js
+++ smart-contracts/migrations/1_initial_migration.js
@@ -1,21 +1,13 @@
 //const Migrations = artifacts.require("Migrations");
-const SsafyNFT = artifacts.require("SsafyNFT");
+const NFTIslandBadge = artifacts.require("NFTIslandBadge");
 // const SsafyToken = artifacts.require("SsafyToken");
-// const SaleFactory = artifacts.require("SaleFactory");
+const SaleFactory = artifacts.require("SaleFactory");
 
-const name = "Non Fungible Token For SSAFY";
-const symbol = "SNFT";
+const name = "Non Fungible Token For NFTIsland";
+const symbol = "BADGE";
 
-/**
- * PJT Ⅰ/Ⅲ - 시나리오 테스트
- * @dev
- * 올바른 테스트를 위해
- * PJT Ⅰ - SsafyNFT
- * PJT Ⅲ - SsafyNFT, SsafyToken, SaleFactory
- * 가 배포되어야 합니다.
- */
 module.exports = function (deployer) {
-  deployer.deploy(SsafyNFT, name, symbol);
+  // deployer.deploy(NFTIslandBadge, name, symbol);
   // deployer.deploy(SsafyToken, "SSAFY", "SSF", 0);
-  // deployer.deploy(SaleFactory);
+  deployer.deploy(SaleFactory, "0x33D9c68D731Ca9fB3fdcE7dda79Fe34FeB33CaE1");
 };
