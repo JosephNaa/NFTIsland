c187c64910f78e561d1a046399d86f0db30dec35
JosephNaa <js.pekah@gmail.com>
2022-03-31 17:37:46 +0900

    [fix] multipartfile 처리 파라미터 수정

--- backend/nfti/src/main/java/com/ssafy/nfti/api/request/CommunityReq.java
+++ backend/nfti/src/main/java/com/ssafy/nfti/api/request/CommunityReq.java
@@ -5,10 +5,12 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 @Data
 @JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class CommunityReq {
+    MultipartFile file;
     String hostAddress;
     String name;
     String description;
