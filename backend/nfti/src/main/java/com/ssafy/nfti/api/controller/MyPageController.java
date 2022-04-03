2233053466324df82f89740005b5bd58bf2f6240
박준용 <dooripjy@naver.com>
2022-04-03 23:54:34 +0900

    update user nickname

--- backend/nfti/src/main/java/com/ssafy/nfti/api/controller/MyPageController.java
+++ backend/nfti/src/main/java/com/ssafy/nfti/api/controller/MyPageController.java
@@ -2,7 +2,9 @@ package com.ssafy.nfti.api.controller;
 
 import com.ssafy.nfti.api.response.CommunityListRes;
 import com.ssafy.nfti.api.response.MyActivityRes;
+import com.ssafy.nfti.api.response.UserRes;
 import com.ssafy.nfti.api.service.MyPageService;
+import com.ssafy.nfti.api.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import java.util.List;
@@ -11,10 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/mypage")
@@ -23,6 +22,9 @@ public class MyPageController {
 
     @Autowired
     MyPageService myPageService;
+
+    @Autowired
+    UserService userService;
 
     @GetMapping("activity")
     @ApiOperation(value = "활동 기록", notes = "내 활동 기록을 불러온다.")
@@ -45,4 +47,20 @@ public class MyPageController {
         List<CommunityListRes> res = myPageService.myCommunityList(pageable, address, onSaleYn);
         return ResponseEntity.ok(res);
     }
+
+    @PutMapping("/{address}")
+    public ResponseEntity<UserRes> updateNickname(
+            @PathVariable String address,
+            @RequestParam String nickname
+    ) {
+//        String url = null;
+//        if (req.getProfile_path() != null) {
+//            url = awss3Service.uploadFile(req.getProfile_path());
+//        }
+
+        UserRes res = userService.updateNickname(address, nickname);
+
+        return ResponseEntity.ok(res);
+    }
+
 }
