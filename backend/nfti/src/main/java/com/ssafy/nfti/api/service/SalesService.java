bb27252f6d0e54461e699a075447a1e387aa30e4
kgw012 <kgw012@gmail.com>
2022-04-04 14:53:49 +0900

    Add listCommunity, listSales, getSale, purchase

--- backend/nfti/src/main/java/com/ssafy/nfti/api/service/SalesService.java
+++ backend/nfti/src/main/java/com/ssafy/nfti/api/service/SalesService.java
@@ -1,10 +1,17 @@
 package com.ssafy.nfti.api.service;
 
 import com.ssafy.nfti.api.request.CreateSaleReq;
+import com.ssafy.nfti.api.request.PurchaseReq;
+import com.ssafy.nfti.api.response.CommunityListRes;
 import com.ssafy.nfti.api.response.SalesRes;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface SalesService {
 
     SalesRes createSales(CreateSaleReq createSaleReq);
-
+    List<CommunityListRes> listCommunitiesOnSale(Pageable pageable);
+    List<SalesRes> listSalesOnCommunityId(Pageable pageable, Long communityId);
+    SalesRes getSale(String saleContractAddress);
+    SalesRes purchase(String saleContractAddress, PurchaseReq req);
 }
---
+++ backend/nfti/src/main/java/com/ssafy/nfti/db/repository/SalesRepositorySupport.java
@@ -0,0 +1,48 @@
+package com.ssafy.nfti.db.repository;
+
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.nfti.common.exception.enums.ExceptionEnum;
+import com.ssafy.nfti.common.exception.response.ApiException;
+import com.ssafy.nfti.db.entity.Community;
+import com.ssafy.nfti.db.entity.Items;
+import com.ssafy.nfti.db.entity.QCommunity;
+import com.ssafy.nfti.db.entity.QItems;
+import com.ssafy.nfti.db.entity.QSales;
+import com.ssafy.nfti.db.entity.Sales;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.EntityManager;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SalesRepositorySupport extends QuerydslRepositorySupport {
+
+    private final JPAQueryFactory jpaQueryFactory;
+    QSales sales = QSales.sales;
+    QItems items = QItems.items;
+
+    public SalesRepositorySupport(EntityManager em) {
+        super(Sales.class);
+        this.jpaQueryFactory = new JPAQueryFactory(em);
+    }
+
+    public List<Sales> findAllSalesOnCommunityId(Pageable pageable, Long communityId) {
+        JPAQuery<Sales> query = jpaQueryFactory
+            .selectFrom(sales)
+            .where(sales.item.tokenId.in(
+                JPAExpressions
+                    .select(items.tokenId)
+                    .from(items)
+                    .where(items.community.id.eq(communityId)
+                        .and(items.onSaleYn.eq(true)))));
+
+
+        return Objects.requireNonNull(getQuerydsl())
+            .applyPagination(pageable, query)
+            .fetch();
+    }
+}
