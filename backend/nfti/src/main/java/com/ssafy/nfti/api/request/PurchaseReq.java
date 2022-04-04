a46b497cf90a3d67fa6e01cceb839430a785ec6b
kgw012 <kgw012@gmail.com>
2022-04-04 14:54:25 +0900

    Add listCommunity, listSales, getSale, purchase

---
+++ backend/nfti/src/main/java/com/ssafy/nfti/api/request/PurchaseReq.java
@@ -0,0 +1,13 @@
+package com.ssafy.nfti.api.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class PurchaseReq {
+    String buyerAddress;
+}
