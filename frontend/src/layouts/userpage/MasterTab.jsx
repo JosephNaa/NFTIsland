bfccd2c72ea8fe713e3adafe6f56ec5060ff4f74
hjyoon <se1620236@naver.com>
2022-04-05 16:28:17 +0900

    Add master tab

--- frontend/src/layouts/userpage/MasterTab.jsx
+++ frontend/src/layouts/userpage/MasterTab.jsx
@@ -1,24 +1,82 @@
-import { Box, Grid } from '@mui/material';
-import CommunityCard from './CommunityCard';
+import { useState } from 'react';
+import { Box, Grid, Typography, CircularProgress } from '@mui/material';
+import PropTypes from 'prop-types';
+import InfiniteScroll from 'react-infinite-scroller';
+import CommunityCard from '../market/CommunityCard';
+import { getUserMasterListAPI } from '../../api/user';
 
-function MasterTab() {
+MasterTab.propTypes = {
+	userName: PropTypes.string.isRequired,
+};
+
+function MasterTab({ userName }) {
+	const [itemInfo, setItemInfo] = useState({
+		communityList: [],
+		hasMoreItems: true,
+	});
+
+	const getUserMasterList = async page => {
+		const { data } = await getUserMasterListAPI('nickname', userName, page, 12);
+		return data;
+	};
+
+	const loadItems = async page => {
+		const data = await getUserMasterList(page);
+		setItemInfo(prev => ({
+			...prev,
+			communityList: prev.communityList.concat(data),
+			hasMoreItems: !!data.length,
+		}));
+	};
+
 	return (
-		<Box maxWidth='xl' sx={{ pt: '30px' }}>
-			<Grid container spacing={6}>
-				<Grid sx={{ mb: 5 }} item xs={12} sm={6} md={4} lg={3}>
-					<CommunityCard />
-				</Grid>
-				<Grid sx={{ mb: 5 }} item xs={12} sm={6} md={4} lg={3}>
-					<CommunityCard />
-				</Grid>
-				<Grid sx={{ mb: 5 }} item xs={12} sm={6} md={4} lg={3}>
-					<CommunityCard />
-				</Grid>
-				<Grid sx={{ mb: 5 }} item xs={12} sm={6} md={4} lg={3}>
-					<CommunityCard />
-				</Grid>
+		<InfiniteScroll
+			pageStart={0}
+			loadMore={loadItems}
+			hasMore={itemInfo.hasMoreItems}
+			loader={
+				<Box
+					sx={{
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+						m: 4,
+					}}
+					key={0}
+				>
+					<CircularProgress />
+				</Box>
+			}
+		>
+			<Box
+				sx={
+					!itemInfo.communityList.length && !itemInfo.hasMoreItems
+						? { p: 16 }
+						: { visibility: 'hidden' }
+				}
+				display='flex'
+				alignItems='center'
+				justifyContent='center'
+			>
+				<Typography sx={{ fontWeight: 'light' }} variant='h3'>
+					There are no items
+				</Typography>
+			</Box>
+			<Grid container spacing={4}>
+				{itemInfo.communityList.map(item => (
+					<Grid item xs={12} sm={6} md={4} lg={3} key={item.id}>
+						<CommunityCard
+							onClickURL={`/community/${item.id}`}
+							communityName={item.name}
+							communityDescription={item.description}
+							communityHost={item.host_nick_name}
+							communityLogo={item.logo_path}
+							hostProfile={item.host_profile}
+						/>
+					</Grid>
+				))}
 			</Grid>
-		</Box>
+		</InfiniteScroll>
 	);
 }
 
