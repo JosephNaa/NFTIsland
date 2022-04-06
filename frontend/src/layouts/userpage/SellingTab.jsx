a6a3668a27e315106ccc62acebc2691f3f297a21
hjyoon <se1620236@naver.com>
2022-04-06 14:02:35 +0900

    Fix selling tab

--- frontend/src/layouts/userpage/SellingTab.jsx
+++ frontend/src/layouts/userpage/SellingTab.jsx
@@ -1,11 +1,46 @@
 import { useState } from 'react';
-import { Box, Grid, FormControl, Select, MenuItem } from '@mui/material';
+import PropTypes from 'prop-types';
+import {
+	Box,
+	Grid,
+	Typography,
+	FormControl,
+	Select,
+	MenuItem,
+	CircularProgress,
+} from '@mui/material';
+import InfiniteScroll from 'react-infinite-scroller';
 import ItemCard from './ItemCard';
+import { getMyOwnItemByNickname } from '../../api/item';
 
-function SellingTab() {
-	const [category, setCategory] = useState('');
+SellingTab.propTypes = {
+	userName: PropTypes.string.isRequired,
+};
+
+function SellingTab({ userName }) {
+	const [category, setCategory] = useState('All');
+	const [itemInfo, setItemInfo] = useState({
+		BadgeList: [],
+		hasMoreItems: true,
+	});
+
 	const handleCategoryChange = event => {
 		setCategory(event.target.value);
+	};
+
+	const loadItems = async page => {
+		const { data } = await getMyOwnItemByNickname(
+			'nickname',
+			userName,
+			true,
+			page,
+			12
+		);
+		setItemInfo(prev => ({
+			...prev,
+			BadgeList: prev.BadgeList.concat(data),
+			hasMoreItems: !!data.length,
+		}));
 	};
 
 	return (
@@ -13,9 +48,6 @@ function SellingTab() {
 			<Box>
 				<FormControl sx={{ m: 1, minWidth: 120 }}>
 					<Select value={category} onChange={handleCategoryChange} displayEmpty>
-						<MenuItem value=''>
-							<em>Community</em>
-						</MenuItem>
 						{/* 가입된 커뮤니티 목록 */}
 						<MenuItem value='All'>All</MenuItem>
 						<MenuItem value='가입된 커뮤니티1'>가입된 커뮤니티1</MenuItem>
@@ -23,22 +55,51 @@ function SellingTab() {
 					</Select>
 				</FormControl>
 			</Box>
-			<Box maxWidth='xl' sx={{ pt: '30px' }}>
-				<Grid container spacing={6}>
-					<Grid sx={{ mb: 5 }} item xs={12} sm={6} md={4} lg={3}>
-						<ItemCard />
-					</Grid>
-					<Grid sx={{ mb: 5 }} item xs={12} sm={6} md={4} lg={3}>
-						<ItemCard />
-					</Grid>
-					<Grid sx={{ mb: 5 }} item xs={12} sm={6} md={4} lg={3}>
-						<ItemCard />
-					</Grid>
-					<Grid sx={{ mb: 5 }} item xs={12} sm={6} md={4} lg={3}>
-						<ItemCard />
-					</Grid>
+			<InfiniteScroll
+				pageStart={0}
+				loadMore={loadItems}
+				hasMore={itemInfo.hasMoreItems}
+				loader={
+					<Box
+						sx={{
+							display: 'flex',
+							justifyContent: 'center',
+							alignItems: 'center',
+							m: 4,
+						}}
+						key={0}
+					>
+						<CircularProgress />
+					</Box>
+				}
+			>
+				<Box
+					sx={
+						!itemInfo.BadgeList.length && !itemInfo.hasMoreItems
+							? { p: 16 }
+							: { visibility: 'hidden' }
+					}
+					display='flex'
+					alignItems='center'
+					justifyContent='center'
+				>
+					<Typography sx={{ fontWeight: 'light' }} variant='h3'>
+						There are no items
+					</Typography>
+				</Box>
+				<Grid container spacing={4}>
+					{itemInfo.BadgeList.map(item => (
+						<Grid item xs={12} sm={6} md={4} lg={3} key={item.token_id}>
+							<ItemCard
+								tokenId={item.token_id}
+								communityName={item.community_name}
+								itemName={item.item_title}
+								itemURL={item.item_url}
+							/>
+						</Grid>
+					))}
 				</Grid>
-			</Box>
+			</InfiniteScroll>
 		</>
 	);
 }
