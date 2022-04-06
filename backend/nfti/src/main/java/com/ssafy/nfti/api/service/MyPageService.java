204bd3fbd9f6bade4471e2629f81bc36e8af76a0
kgw012 <kgw012@gmail.com>
2022-04-06 15:25:32 +0900

    Update myActivityList (add communityId filter)

--- backend/nfti/src/main/java/com/ssafy/nfti/api/service/MyPageService.java
+++ backend/nfti/src/main/java/com/ssafy/nfti/api/service/MyPageService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.data.domain.Pageable;
 
 public interface MyPageService {
-    List<MyActivityRes> myActivityList(Pageable pageable, String findBy, String search);
+    List<MyActivityRes> myActivityList(Pageable pageable, String findBy, String search, Long communityId);
     List<CommunityListRes> myCommunityList(Pageable pageable, String findBy, String search, Boolean onSaleYn);
     List<CommunityListRes> myCommunityMasterList(Pageable pageable, String findBy, String search);
 }
